726321b318a3ad305ba3b211bb3edf9345f851f6
xpanpanda <140177939+xpanpanda@users.noreply.github.com>
2024-03-30 21:20:06 +0800

    Update motionctrl.py

--- motionctrl/motionctrl.py
+++ motionctrl/motionctrl.py
@@ -26,18 +26,23 @@ class MotionCtrl(LatentDiffusion):
 
 
         # camera motion control module
-
         bound_method = selfattn_forward_unet.__get__(
             self.model.diffusion_model,
             self.model.diffusion_model.__class__)
+        # 使用 selfattn_forward_unet.__get__ 方法
+        # 绑定 selfattn_forward_unet 函数到 diffusion_model 的 forward 方法。
         setattr(self.model.diffusion_model, 'forward', bound_method)
 
         for _name, _module in self.model.diffusion_model.named_modules():
+            # 如果模块是 TemporalTransformer 类型，
+            # 则绑定 TemporalTransformer_forward 方法到模块的 forward 方法。
             if _module.__class__.__name__ == 'TemporalTransformer':
                 bound_method = TemporalTransformer_forward.__get__(
                     _module, _module.__class__)
                 setattr(_module, 'forward', bound_method)
-
+                
+            # 如果模块是 BasicTransformerBlock 类型，
+            # 则根据模块是否包含空间注意力（spatial_selfattn）和时间注意力（temporal_selfattn）来决定绑定哪种前向传播方法：
             if _module.__class__.__name__ == 'BasicTransformerBlock':
                 # SpatialTransformer only
                 if _module.attn2.to_k.in_features != context_dim: # TemporalTransformer without crossattn 
